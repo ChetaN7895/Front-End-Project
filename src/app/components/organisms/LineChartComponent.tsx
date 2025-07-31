5f49a003b4880005326f09f4a1e3ca9f2880776e
Chetan Solanki <151900157+ChetaN7895@users.noreply.github.com>
2025-07-31 19:26:25 +0530

    Update LineChartComponent.tsx

--- src/app/components/organisms/LineChartComponent.tsx
+++ src/app/components/organisms/LineChartComponent.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
-import { YearlySales } from '@/types/sales';
+import { YearlySales } from '../types/sales';
 
 interface LineChartComponentProps {
   data: YearlySales[];
